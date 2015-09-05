3eab288fb7e3785108b88e4adf163a8452d673d2
Andreas Karlsen <apk@netcompany.com>
2015-09-05 15:31:57 +0200

    Added unit tests for the point class

---
+++ CSharp6/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6
+{
+    public class Point
+    {
+        private int _X;
+        public int X
+        {
+            get
+            {
+                return _X;
+            }
+        }
+        public int Y { get; private set; }
+
+        public Point(int x, int y)
+        {
+            _X = x;
+            Y = y;
+        }
+
+        public double Dist
+        {
+            get
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", X, Y);
+        }
+    }
+}
---
+++ CSharp6Test/PointTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp6;
+
+namespace CSharp6Test
+{
+    [TestClass]
+    public class PointTest
+    {
+        Point _point;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _point = new Point(0, 16);
+        }
+
+        [TestMethod]
+        public void TestGetter()
+        {
+            Assert.AreEqual(0, _point.X);
+            Assert.AreEqual(16, _point.Y);
+        }
+
+        [TestMethod]
+        public void TestSetter()
+        {
+            //_point.X = 3;
+        }
+
+        [TestMethod]
+        public void TestDistance()
+        {
+            Assert.AreEqual(16, _point.Dist);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            Assert.AreEqual("(0, 16)", _point.ToString());
+        }
+    }
+}
